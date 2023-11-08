2d78fdffc3ad103eb5af906b2411635fe1fc9ca4
Feliks <kg.fox002@gmail.com>
2023-11-08 16:51:33 +0600

    dz1 changed

--- app.js
+++ app.js
@@ -26,18 +26,53 @@ switch (lowerCaseColor) {
     alert("Неизвестный цвет светофора. Пожалуйста, введите красный, желтый или зеленый.");
 }
 
+
 // Конвертер чисел
 const number = parseInt(prompt("Введите число от 1 до 9:"));
 
-function convertToRoman(num) {
-  const romanNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-  if (num >= 1 && num <= 9) {
-    return romanNumerals[num - 1];
-  } else {
-    return "Некорректное число";
-  }
+// function convertToRoman(num) {
+//   const romanNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+//   if (num >= 1 && num <= 9) {
+//     return romanNumerals[num  - 1];
+//   } else {
+//     return "Некорректное число";
+//   }
+// }
+
+// const romanNumber = convertToRoman(number);
+
+
+// alert(`Римское представление числа ${number} - ${romanNumber}`);
+
+switch (number) {
+  case 1:
+    alert("I");
+    break;
+  case 2:
+    alert("II");
+    break;
+  case 3:
+    alert("III");
+    break;
+  case 4:
+    alert("IV");
+    break;
+  case 5:
+    alert("V");
+    break;
+  case 6:
+    alert("VI");
+    break;
+  case 7:
+    alert("VII");
+    break;
+  case 8:
+    alert("VIII");
+    break;
+  case 9:
+    alert("IX");
+    break;
+  default:
+    alert("Некорректное число");
+
 }
-
-const romanNumber = convertToRoman(number);
-
-alert(`Римское представление числа ${number} - ${romanNumber}`);
